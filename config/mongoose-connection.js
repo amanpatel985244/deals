36ebaeb27013bf13caa7d7105314e122a51fb22a
Aman kumar patel <amanpatel985244@gmail.com>
2025-06-20 18:57:39 +0530

    Fix: Use environment variable for MongoDB URI

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,30 +1,36 @@
-const mongoose = require("mongoose");
-const config = require("config");
-const dbgr = require("debug")("development : mongoose");
-
-
-mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
-.then(function(){
-    dbgr("connected");
-})
-
-.catch(function(err){
-    dbgr(err);
-});
-
-module.exports = mongoose.connection;
-
 // const mongoose = require("mongoose");
+// const config = require("config");
 // const dotenv = require("dotenv");
-// dotenv.config();
+// const dbgr = require("debug")("development : mongoose");
 
+
+// // mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
+// // .then(function(){
+// //     dbgr("connected");
+// // })
+
+// // .catch(function(err){
+// //     dbgr(err);
+// // });
 // mongoose.connect(process.env.MONGODB_URI, {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
-// });
+// })
+//   .then(() => console.log("✅ Connected to MongoDB"))
+//   .catch((err) => console.error("❌ MongoDB connection error:", err));
+// module.exports = db;
 
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "MongoDB connection error:"));
-// db.once("open", () => console.log("✅ Connected to MongoDB Atlas"));
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
-// module.exports = mongoose.connection;
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => console.log("✅ Connected to MongoDB Atlas"));
+
+module.exports = mongoose.connection;
